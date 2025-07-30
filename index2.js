0d32f27cab3711b46aa7c6b7fdd6192390b4e1ae
NAYUJIN-svg <won10ve8953@gmail.com>
2025-07-30 09:18:18 +0900

    javascript_0730_editing
    javascript_0730_editing

---
+++ index2.js
@@ -0,0 +1,12 @@
+var d = document //변수선언 바깥에서도 쓸 수있는 변수
+let num1 = 5; //변수선언 변할수 있는 수
+let num2 = 3; //변수선언 변할수 있는 수
+const num3 = 8; // 상수선언 변하지않는 수 
+
+console.log(typeof 42); //number
+console.log(typeof null); // object
+console.log(typeof true); // boolean
+console.log(typeof "hello"); //string
+
+//typeof는 데이터의 타입(자료형을) 확인할 때 사용하는 연산자
+
