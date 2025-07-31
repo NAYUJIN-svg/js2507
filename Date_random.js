55c8b9b3cf50ecb9af7575e3cd7494e22c31e367
NAYUJIN-svg <won10ve8953@gmail.com>
2025-07-31 17:05:36 +0900

    0731
    0731

---
+++ Date_random.js
@@ -0,0 +1,15 @@
+const now = new Date();
+const randomHour = Math.floor(Math.random() * 24); //0~23
+const randomMinute = Math.floor(Math.random() * 60); //0~59
+const randomSecond = Math.floor(Math.random() * 60); //0~59
+
+const randomTime = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    randomHour,
+    randomMinute,
+    randomSecond
+);
+
+console.log("오늘의 무작위 시각:", randomTime.toLocaleString());
